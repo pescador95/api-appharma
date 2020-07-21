042f3ee724c91f038a31b10befcf352d81ca9ddc
breitenbach <cesar.breitenbach@gmail.com>
2020-07-20 21:55:48 -0300

    subindo

--- src/server.js
+++ src/server.js
@@ -5,16 +5,16 @@ const http = require('http')
 import fs from 'fs'
 const port = 3333;
 
-// const options = {
-//    key: fs.readFileSync(resolve(__dirname, 'config', 'local-key.pem'), 'utf-8'),
-//    cert: fs.readFileSync(resolve(__dirname, 'config', 'local-cert.pem'), 'utf-8')
-// }
+const options = {
+   key: fs.readFileSync(resolve(__dirname, 'config', 'privkey.pem'), 'utf-8'),
+   cert: fs.readFileSync(resolve(__dirname, 'config', 'fullchain.pem'), 'utf-8')
+}
 
-// https.createServer(options, app)
-//     .listen(port, () => {
-//         console.log(`enviroment: ${process.env.APP_ENV}`)
-//         console.log(`estamos online na porta  ${port}`)
-//     })
+https.createServer(options, app)
+    .listen(port, () => {
+        console.log(`enviroment: ${process.env.APP_ENV}`)
+        console.log(`estamos online na porta  ${port}`)
+    })
 
 http.createServer(app)
     .listen(port, () => {
