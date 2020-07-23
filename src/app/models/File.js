123f9ef0d74b82c86a37016cf1b2c5c234f86eda
breitenbach <cesar.breitenbach@gmail.com>
2020-07-22 23:01:09 -0300

    problema no url da imagem

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
          url: {
             type: Sequelize.VIRTUAL,
             get() {
-               return `${process.env.app_url}/files/${this.path}`
+               return `${process.env.app_url?process.env.app_url:'https://ioffertas.club:3333'}/files/${this.path}`
             }
          }
       },
