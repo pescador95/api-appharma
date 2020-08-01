bcd00e11aa63d0fbe094e80e9f344bad7544cb66
breitenbach <cesar.breitenbach@gmail.com>
2020-08-01 10:33:14 -0300

    filtro de arquivos imagem

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,6 +6,12 @@ import { extname, resolve} from 'path'
 export default {
    storage: multer.diskStorage({
       destination: resolve(__dirname, '..', '..',  'tmp', 'uploads'),
+      fileFilter: function (req, file, cb) {
+         if ((file.mimetype != 'image/jpeg') && (file.mimetype != 'image/png') && (file.mimetype != 'image/svg+xml') && (file.mimetype != 'image/png') ){
+             return cb(new Error('Tipo de arquivo invalido'));
+         }
+         cb(null, true)
+       },
       filename: (req, fil, cb) =>{
          crypto.randomBytes(16, (err, res) =>{
             if(err) return cb(err)
