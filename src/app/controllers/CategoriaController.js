291fc94dddd009d64b9f2e5a375b89079929344f
breitenbach <cesar.breitenbach@gmail.com>
2020-08-05 16:10:25 -0300

    rota pra add imagem na categoria via put

--- src/app/controllers/CategoriaController.js
+++ src/app/controllers/CategoriaController.js
@@ -2,30 +2,31 @@ import Categoria from '../models/Categoria'
 import Service from '../services/CategoriaService'
 
 class CategoriaController {
-   async store (req, res) {
+   async store(req, res) {
 
-      const {descricao} = req.body
-      
-      const categoria = await Categoria.create({descricao})
+      const { descricao } = req.body
 
-      if (!categoria){
-         return res.status(400).json({error:"Ocorreu um erro ao inserir a categoria"})
+      const categoria = await Categoria.create({ descricao })
+
+      if (!categoria) {
+         return res.status(400).json({ error: "Ocorreu um erro ao inserir a categoria" })
       }
 
-      const {id} = categoria
+      const { id } = categoria
 
-      return res.json({id, descricao})
+      return res.json({ id, descricao })
    }
 
    async update(req, res) {
       if (!req.userAdmin) {
          return res.status(401).json({ error: "permitido apenas para administradores" })
       }
-
-      const categoria = await Service.CategoriaExiste({ idCategoria: req.query.id })
-
-      res.json(categoria)
-
+      try {
+         const categoria = await Service.CategoriaExiste({ idCategoria: req.query.id })
+         res.json(categoria)
+      } catch (e) {
+         res.status(400).json({ error: e.message })
+      }
    }
 }
 
