8d91eab46cf13c2347da61e9d8c3bab6c0c9de5d
breitenbach <cesar.breitenbach@gmail.com>
2020-08-08 19:23:22 -0300

    adicionando filtro ao sql de simliars

--- src/app/controllers/ProdutoController.js
+++ src/app/controllers/ProdutoController.js
@@ -24,125 +24,127 @@ class ProdutoController {
       if (!produto) {
          return res.status(400).json({ error: "Produto não encontrado" })
       }
-      
+
       return res.json(produto.rows)
-      
+
    }
-   
+
    async topSellers(req, res) {
-      
+
       const params = ['2020-04-28T00:00:00-03', '2020-01-01T00:00:00-03', '2020-04-30T00:00:00-03']
       const sql = " SELECT tmp.id, tmp.nome, tmp.preco_vigente, tmp.preco_original, tmp.preco_promocao, image, discount, COUNT(*) AS total FROM ( " +
-      "                                                                                                                                                              " +
-      " SELECT p.id , p.codigo_barras, p.nome, p.descricao,                                                                                                           " +
-      "        COALESCE(p1.preco_promocao, p.valor_venda) AS preco_vigente, p.valor_venda as preco_original, p1.preco_promocao, f.path AS image, p.principio,        " +
-      "        COALESCE((1 - p1.preco_promocao / p.valor_venda)*100, 0) AS discount                                                                                        " +
-      "     FROM produtos p                                                                                                                                          " +
-      "   left JOIN promocoes p1 ON p.id = p1.id_produto  and p1.data_inicio < $1 and p1.data_fim > $1                                                            " +
-      "   LEFT JOIN files f ON p.img_id = f.id                                                                                                                       " +
-      "   INNER JOIN vendas v ON p.id = v.id_produto                                                                                                                 " +
-      "   LEFT JOIN tipo_produto tp ON p.id_tipo = tp.id                                                                                                             " +
-      "   WHERE v.data_venda BETWEEN $2 AND $3 AND p1.codigo_barras <> '12345679'                                                                " +
-      "   ) tmp                                                                                                                                                      " +
-      "                                                                                                                                                              " +
-      "   GROUP BY  tmp.id, tmp.nome, tmp.preco_vigente, tmp.preco_original, tmp.preco_promocao,  image, discount                       " +
-      "   ORDER BY total desc                                                                                                                                        " +
-      "                                                                                                                                                              " +
-      "   LIMIT 15                                                                                                                                                   ";
-      
+         "                                                                                                                                                              " +
+         " SELECT p.id , p.codigo_barras, p.nome, p.descricao,                                                                                                           " +
+         "        COALESCE(p1.preco_promocao, p.valor_venda) AS preco_vigente, p.valor_venda as preco_original, p1.preco_promocao, f.path AS image, p.principio,        " +
+         "        COALESCE((1 - p1.preco_promocao / p.valor_venda)*100, 0) AS discount                                                                                        " +
+         "     FROM produtos p                                                                                                                                          " +
+         "   left JOIN promocoes p1 ON p.id = p1.id_produto  and p1.data_inicio < $1 and p1.data_fim > $1                                                            " +
+         "   LEFT JOIN files f ON p.img_id = f.id                                                                                                                       " +
+         "   INNER JOIN vendas v ON p.id = v.id_produto                                                                                                                 " +
+         "   LEFT JOIN tipo_produto tp ON p.id_tipo = tp.id                                                                                                             " +
+         "   WHERE v.data_venda BETWEEN $2 AND $3 AND p1.codigo_barras <> '12345679'                                                                " +
+         "   ) tmp                                                                                                                                                      " +
+         "                                                                                                                                                              " +
+         "   GROUP BY  tmp.id, tmp.nome, tmp.preco_vigente, tmp.preco_original, tmp.preco_promocao,  image, discount                       " +
+         "   ORDER BY total desc                                                                                                                                        " +
+         "                                                                                                                                                              " +
+         "   LIMIT 15                                                                                                                                                   ";
 
-      try{
+
+      try {
          const lista = await db.query(sql, params)
-         if(!lista){
-            return res.status(400).json({error:'Impossivel pegar produtos'})
+         if (!lista) {
+            return res.status(400).json({ error: 'Impossivel pegar produtos' })
          }
-         
+
          return res.json(lista.rows)
-      }catch(e) {
-         return res.status(400).json({error:e.message})
+      } catch (e) {
+         return res.status(400).json({ error: e.message })
       }
-   
-}
 
-async similars(req, res) {
+   }
 
-   const {tipo} = req.query;
-      
-   const params = ['2020-04-28T00:00:00-03', tipo]
-   const sql = " SELECT p.id , p.codigo_barras, p.nome, p.descricao,                                                                                                        "+ 
-   "              COALESCE(p1.preco_promocao, p.valor_venda) AS preco_vigente, p.valor_venda as preco_original, p1.preco_promocao, f.path AS image, p.principio,"+ 
-   "               COALESCE((1 - p1.preco_promocao / p.valor_venda)*100, 0) AS discount                                                                         "+
-   "                                                                                                                                                            "+
-   "   FROM produtos p                                                                                                                                          "+
-   "   left JOIN promocoes p1 ON p.id = p1.id_produto and p1.data_inicio < $1 and p1.data_fim > $1                                                              "+
-   "   LEFT JOIN files f ON p.img_id = f.id                                                                                                                     "+
-   "   WHERE p.id_tipo = $2                                                                                                                                     ";
-   
+   async similars(req, res) {
 
-   try{
-      const lista = await db.query(sql, params)
-      if(!lista){
-         return res.status(400).json({error:'Impossivel pegar produtos'})
+      const { tipo } = req.query;
+
+      const params = ['2020-04-28T00:00:00-03', tipo]
+      const sql =
+         "SELECT p.id , p.codigo_barras, p.nome, p.descricao,                                                                                                             " +
+         "                 COALESCE(p1.preco_promocao, p.valor_venda) AS preco_vigente, p.valor_venda as preco_original, p1.preco_promocao, f.path AS image, p.principio, " +
+         "                  COALESCE((1 - p1.preco_promocao / p.valor_venda)*100, 0) AS discount                                                                          " +
+         "      FROM produtos p                                                                                                                                           " +
+         "      left JOIN promocoes p1 ON p.id = p1.id_produto and p1.data_inicio < $1 and p1.data_fim > $1                                                               " +
+         "      LEFT JOIN files f ON p.img_id = f.id                                                                                                                      " +
+         "      INNER JOIN estoque e ON p.id = e.id_produto                                                                                                               " +
+         "      WHERE p.id_tipo = $2 AND e.qtdestoque > 0                                                                                                                 " +
+         "  ORDER BY discount DESC, preco_vigente asc                                                                                                                     ";
+
+
+      try {
+         const lista = await db.query(sql, params)
+         if (!lista) {
+            return res.status(400).json({ error: 'Impossivel pegar produtos' })
+         }
+
+         return res.json(lista.rows)
+      } catch (e) {
+         return res.status(400).json({ error: e.message })
       }
-      
-      return res.json(lista.rows)
-   }catch(e) {
-      return res.status(400).json({error:e.message})
+
    }
 
-}
 
+   async index(req, res) {
+      const { barra } = req.params
 
-async index(req, res) {
-   const { barra } = req.params
+      const produto = await ProdutoService.ProdutoExiste({ codigoBarras: barra })
 
-   const produto = await ProdutoService.ProdutoExiste({ codigoBarras: barra })
+      if (!produto) {
+         return res.status(400).json({ error: "Poduto não encontrado" })
+      }
 
-   if (!produto) {
-      return res.status(400).json({ error: "Poduto não encontrado" })
+      return res.status(200).json(produto)
+
    }
 
-   return res.status(200).json(produto)
+   async show(req, res) {
 
-}
+      const { page = 1 } = req.query
 
-async show(req, res) {
+      const produto = await Produto.findAll({
+         order: ['nome'],
+         attributes: ['codigo_barras', 'nome', 'valor_venda'],
+         limit: 20,
+         offset: (page - 1) * 20,
+      })
 
-   const { page = 1 } = req.query
-
-   const produto = await Produto.findAll({
-      order: ['nome'],
-      attributes: ['codigo_barras', 'nome', 'valor_venda'],
-      limit: 20,
-      offset: (page - 1) * 20,
-   })
-
-   res.json(produto)
-}
-
-async store(req, res) {
-
-   if (!req.userAdmin) {
-      return res.status(401).json({ error: "Cadastro permitido apenas para administradores" })
+      res.json(produto)
    }
 
-   const produto = await Produto.create(req.body)
+   async store(req, res) {
 
-   if (!produto) {
-      return res.status(400).json({ error: "Não foi possivel criar o produto" })
+      if (!req.userAdmin) {
+         return res.status(401).json({ error: "Cadastro permitido apenas para administradores" })
+      }
+
+      const produto = await Produto.create(req.body)
+
+      if (!produto) {
+         return res.status(400).json({ error: "Não foi possivel criar o produto" })
+      }
+
+      return res.status(201).json(produto)
    }
 
-   return res.status(201).json(produto)
-}
-
-async update(req, res) {
-   if (!req.userAdmin) {
-      return res.status(401).json({ error: "permitido apenas para administradores" })
+   async update(req, res) {
+      if (!req.userAdmin) {
+         return res.status(401).json({ error: "permitido apenas para administradores" })
+      }
+      const produto = await ProdutoService.ProdutoExiste({ idProduto: req.query.id })
+      const produtoAtt = await produto.update(req.body)
+      return res.status(201).json(produtoAtt)
    }
-   const produto = await ProdutoService.ProdutoExiste({ idProduto: req.query.id })
-   const produtoAtt = await produto.update(req.body)
-   return res.status(201).json(produtoAtt)
-}
 
 }
 export default new ProdutoController()
