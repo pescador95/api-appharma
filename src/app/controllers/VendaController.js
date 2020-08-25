701312e9736eb816efafe3b7cf96cfac8d1652d7
breitenbach <cesar.breitenbach@gmail.com>
2020-08-25 20:32:41 -0300

    testes

--- src/app/controllers/VendaController.js
+++ src/app/controllers/VendaController.js
@@ -4,18 +4,20 @@ import { v4 as uuidv4 } from 'uuid';
 class VendaController {
 
    async store(req, res){
-      try{
-         const uuid = uuidv4();
-         const {cart} = req.body;
 
-         map.cart((i,k) =>{
-            console.log(`uudi da venda: ${uuid} produto: ${i.nome} quantidade no carrinho: ${i.qtd}`)
-         })
-         //const venda = Venda.create({codigo_venda:uuid, cpf, codigo_barras, id_produto, })
-         //return res.json(venda)
-      } catch(e) {
-         return res.status(500).json({error:'Internal server error'})
-      }
+      return res.json(req.body)
+      // try{
+      //    const uuid = uuidv4();
+      //    const {cart} = req.body;
+
+      //    map.cart((i,k) =>{
+      //       console.log(`uudi da venda: ${uuid} produto: ${i.nome} quantidade no carrinho: ${i.qtd}`)
+      //    })
+      //    //const venda = Venda.create({codigo_venda:uuid, cpf, codigo_barras, id_produto, })
+      //    //return res.json(venda)
+      // } catch(e) {
+      //    return res.status(500).json({error:'Internal server error'})
+      // }
 
    }
 
