c2f342e7b68a2a2825f40cf5a65c1cb683ac237b
breitenbach <cesar.breitenbach@gmail.com>
2020-08-25 21:54:01 -0300

    testes

--- src/database/migrations/20200729012716-create_vendas.js
+++ src/database/migrations/20200729012716-create_vendas.js
@@ -11,7 +11,7 @@ module.exports = {
             autoIncrement: true,
          },
          codigo_venda:{
-            type:Sequelize.INTEGER,
+            type:Sequelize.STRING,
             allowNull:false,
          },
          cpf: {
