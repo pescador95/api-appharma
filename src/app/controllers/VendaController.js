d92f40507a99083203b0707bf4b478ba3ad7d0f2
breitenbach <cesar.breitenbach@gmail.com>
2020-08-25 21:58:46 -0300

    testes

--- src/app/controllers/VendaController.js
+++ src/app/controllers/VendaController.js
@@ -4,23 +4,29 @@ import { v4 as uuidv4 } from 'uuid';
 
 class VendaController {
 
-   async store(req, res){
-      const {cart, levar_pinpad, troco_para, tipo_venda = A} = req.body
+   async store(req, res) {
+      const { cart, levar_pinpad, troco_para, tipo_venda = A } = req.body
       const now = new Date()
       const uuid = uuidv4()
       const userId = req.userId
       const usuario = await Usuario.findByPk(userId)
-      const {cpf} = usuario;
+      const { cpf } = usuario;
 
-      cart.map((i,k) =>{
-               const {codigo_barras, nome, preco_vigente, id } = cart
-               console.log(`vou inserir a venda do item: codigo de barra: ${codigo_barras}  nome: ${nome} valor: ${preco_vigente} id: ${id}`)
-               for(let j=0; j < i.qtd; j++ ){
-                  Venda.create({codigo_venda:uuid, user_id:userId, id_produto:id, codigo_barras, nome, valor_liquido:preco_vigente, cpf, created_at:now, updated_at:now, data_venda:now, tipo_venda, levar_pinpad, troco_para})
-               }
-             })
+      try {
 
-      return res.status(201).json({sucesso:"Venda efetuada com sucesso", codigo_venda:uuid})
+         cart.map((i, k) => {
+            const { codigo_barras, nome, preco_vigente, id } = i
+            console.log(`vou inserir a venda do item: codigo de barra: ${codigo_barras}  nome: ${nome} valor: ${preco_vigente} id: ${id}`)
+            for (let j = 0; j < i.qtd; j++) {
+               Venda.create({ codigo_venda: uuid, user_id: userId, id_produto: id, codigo_barras, nome, valor_liquido: preco_vigente, cpf, created_at: now, updated_at: now, data_venda: now, tipo_venda, levar_pinpad, troco_para })
+            }
+         })
+
+         return res.status(201).json({ sucesso: "Venda efetuada com sucesso", codigo_venda: uuid })
+      } catch (e) {
+         console.log(e.message)
+         return res.status(500).json({ erro: e.message })
+      }
    }
 
 }
