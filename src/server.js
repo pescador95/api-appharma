9b0d73bf9c8e6add1de2656aeb2e0c38a6d714b1
breitenbach <cesar.breitenbach@gmail.com>
2020-09-26 02:29:57 -0300

    adicionando socket.io no servidor

--- src/server.js
+++ src/server.js
@@ -4,27 +4,45 @@ const https = require('https')
 const http = require('http')
 import fs from 'fs'
 const port = 3333;
+const io = require('socket.io')
 
 let existe = fs.existsSync(resolve(__dirname, 'config', 'privkey.pem'));
 
-if ( existe ) {  
+if (existe) {
    console.log("Existe chave...")
    const options = {
       key: fs.readFileSync(resolve(__dirname, 'config', 'privkey.pem'), 'utf-8'),
       cert: fs.readFileSync(resolve(__dirname, 'config', 'fullchain.pem'), 'utf-8')
    }
 
-   https.createServer(options, app)
+   const server = https.createServer(options, app)
       .listen(port, () => {
          console.log(`enviroment: ${process.env.APP_ENV}`)
          console.log(`estamos online na porta  ${port}`)
       })
+
+   const socket = io(server)
+
+   socket.on('connection', (socket) => {
+      console.log("Recebendo conexão do cliente...")
+   })
+
+
 } else {
 
-console.log("Não existe chave...")
-http.createServer(app)
-   .listen(port, () => {
-      console.log(`enviroment: ${process.env.APP_ENV}`)
-      console.log(`estamos online na porta  ${port}`)
+   console.log("Não existe chave...")
+   const server = http.createServer(app)
+      .listen(port, () => {
+         console.log(`enviroment: ${process.env.APP_ENV}`)
+         console.log(`estamos online na porta  ${port}`)
+      })
+   const socket = io(server)
+
+   socket.on('connection', (socket) => {
+      console.log("Recebendo conexão do cliente...")
    })
+
+
+
+
 }
