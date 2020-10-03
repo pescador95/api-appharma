c70195c8a5efb1a383aa81c18be2e89210f7f64c
breitenbach <cesar.breitenbach@gmail.com>
2020-10-02 21:58:39 -0300

    correcoes

--- src/app/services/Atualizador.js
+++ src/app/services/Atualizador.js
@@ -3,9 +3,18 @@ import useDiff from '../services/UseDiff'
 
 const Atualizador = {
    atualizaEstoque: async (tabelaAtualizada) => {
+      console.log("REcebi a tabela: "+JSON.stringify(tabelaAtualizada))
       const estoque = useEstoque();
       const diff = useDiff()
+      console.log("--------------------- entrar no get tabela ----------------------")
       const tabelaEstoque = await estoque.getTabela()
+      if(tabelaAtualizada){
+         console.log("Tenho tabela atualizada")
+      }
+      if(tabelaEstoque){
+         console.log("tenho tabela de estoque velha")
+      }
+      
       await diff.verificaEstoque(tabelaAtualizada, tabelaEstoque);
       console.log('socket finalizado')
    }
