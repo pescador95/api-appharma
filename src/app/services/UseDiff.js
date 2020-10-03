2cb718d5bc8d26171bc9bda12a5b9b26cf6c78be
breitenbach <cesar.breitenbach@gmail.com>
2020-10-03 18:55:44 -0300

    tst

--- src/app/services/UseDiff.js
+++ src/app/services/UseDiff.js
@@ -4,7 +4,6 @@ const UseDiff = {
    verificaEstoque: async (tabelaAtualizada, tabelaAntiga) => {
       console.log("Vou verificar as diferenças entre as 2 tabelas que eu recebi....")
       const estoque = useEstoque()
-      let cont;
       const promise = tabelaAntiga.map(async (i, k) => {
 
          const itemNovo = await tabelaAtualizada.filter(novo_item => novo_item.codigo_barras == i.codigo_barras);
@@ -12,38 +11,42 @@ const UseDiff = {
          if (!itemNovo) {
             return
          }
-
-         if (typeof itemNovo === 'undefined') {
+         
+         if (typeof  itemNovo[0].qtd_estoque === 'undefined') {
             return
-          }
+         }
+         if (typeof itemNovo[0].preco_venda === 'undefined') {
+            return
+         }
+         if (typeof itemNovo[0].preco_promocao === 'undefined') {
+            return
+         }
+         
+         //FIZ ISSO PRA TENTAR ACHAR ONDE ESTÁ O UNDEFINED
+        let estoqueAtt =  itemNovo[0].qtd_estoque
+        let precoVendaAtt = itemNovo[0].preco_venda
+        let precoPromoAtt = itemNovo[0].preco_promocao
 
+         if (parseFloat(i.qtd_estoque) != parseFloat(estoqueAtt)) {
+            console.log(`vou atualizar o ${JSON.stringify(itemNovo)}`)
+            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
+            return
+         }
 
-         let estoqueAtt =  itemNovo[0].qtd_estoque
-         let precoVendaAtt = itemNovo[0].preco_venda
-         let precoPromoAtt = itemNovo[0].preco_promocao
-         console.log(`Esses são os valores quee u preciso att: ${estoqueAtt} ${precoVendaAtt} e ${precoPromoAtt} é isso.. fuck!`)
-         cont++;
+         if (parseFloat(i.preco_venda) != parseFloat(precoVendaAtt)) {
+            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
+            return
+         }
 
-         // if (parseFloat(i.qtd_estoque) != parseFloat(estoqueAtt)) {
-         //    console.log(`vou atualizar o ${JSON.stringify(itemNovo)}`)
-         //    await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
-         //    return
-         // }
-
-         // if (parseFloat(i.preco_venda) != parseFloat(precoVendaAtt)) {
-         //    await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
-         //    return
-         // }
-
-         // if (parseFloat(i.preco_promocao) != parseFloat(precoPromoAtt)) {
-         //    await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
-         //    return
-         // }
+         if (parseFloat(i.preco_promocao) != parseFloat(precoPromoAtt)) {
+            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
+            return
+         }
 
       })
 
       await Promise.all(promise)
-      console.log("Atualizei tabela de estoque... e o contador é: "+cont)
+      console.log("Atualizei tabela de estoque...")
    }
 
 }
