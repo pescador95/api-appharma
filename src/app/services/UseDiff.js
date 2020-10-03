0ea75865433099a93474e9a2d90bc4cc1eaf914f
breitenbach <cesar.breitenbach@gmail.com>
2020-10-02 22:29:49 -0300

    fuck

--- src/app/services/UseDiff.js
+++ src/app/services/UseDiff.js
@@ -14,17 +14,17 @@ const UseDiff = {
          console.log("AChei item novo: "+JSON.stringify(itemNovo))
 
          if (parseFloat(i.qtdestoque) != parseFloat(itemNovo[0].qtd_estoque)) {
-            await estoque.update({ codigo_barras: item[0].codigo_barras, preco_venda: item[0].preco_venda, preco_promocao: item[0].preco_promocao, qtd_estoque: item[0].qtd_estoque })
+            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: itemNovo[0].preco_venda, preco_promocao: itemNovo[0].preco_promocao, qtd_estoque: itemNovo[0].qtd_estoque })
             return
          }
    
-         if (parseFloat(i.preco_venda) != parseFloat(item[0].preco_venda)) {
-            await estoque.update({ codigo_barras: item[0].codigo_barras, preco_venda: item[0].preco_venda, preco_promocao: item[0].preco_promocao, qtd_estoque: item[0].qtd_estoque })
+         if (parseFloat(i.preco_venda) != parseFloat(itemNovo[0].preco_venda)) {
+            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: itemNovo[0].preco_venda, preco_promocao: itemNovo[0].preco_promocao, qtd_estoque: itemNovo[0].qtd_estoque })
             return
          }
    
-         if (parseFloat(i.preco_promocao) != parseFloat(item[0].preco_promocao)) {
-            await estoque.update({ codigo_barras: item[0].codigo_barras, preco_venda: item[0].preco_venda, preco_promocao: item[0].preco_promocao, qtd_estoque: item[0].qtd_estoque })
+         if (parseFloat(i.preco_promocao) != parseFloat(itemNovo[0].preco_promocao)) {
+            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: itemNovo[0].preco_venda, preco_promocao: itemNovo[0].preco_promocao, qtd_estoque: itemNovo[0].qtd_estoque })
             return
          }
    
