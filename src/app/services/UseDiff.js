088c7832cb6d9048fcc960209a568dfb2db57ccd
breitenbach <cesar.breitenbach@gmail.com>
2020-10-03 20:01:04 -0300

    feita a correcao de logica

--- src/app/services/UseDiff.js
+++ src/app/services/UseDiff.js
@@ -2,44 +2,42 @@ import useEstoque from '../controllers/EstoqueController'
 
 const UseDiff = {
    verificaEstoque: async (tabelaAtualizada, tabelaAntiga) => {
-      console.log("Vou verificar as diferenças entre as 2 tabelas que eu recebi....")
+      console.log("Analisando tabelas e atualizando diferenças.")
       const estoque = useEstoque()
-      const promise = tabelaAntiga.map(async (i, k) => {
+      const promise = tabelaAtualizada.map(async (i, k) => {
 
-         const itemNovo = await tabelaAtualizada.filter(novo_item => novo_item.codigo_barras == i.codigo_barras);
+         const itemAtual = await tabelaAntiga.filter(item => item.codigo_barras == i.codigo_barras);
 
-         if (!itemNovo) {
+         if (!itemAtual) {
             return
          }
          
-         if (typeof  itemNovo[0].qtd_estoque === 'undefined') {
+         if (typeof  itemAtual[0].qtd_estoque === 'undefined') {
             return
          }
-         if (typeof itemNovo[0].preco_venda === 'undefined') {
+         if (typeof itemAtual[0].preco_venda === 'undefined') {
             return
          }
-         if (typeof itemNovo[0].preco_promocao === 'undefined') {
+         if (typeof itemAtual[0].preco_promocao === 'undefined') {
             return
          }
          
-         //FIZ ISSO PRA TENTAR ACHAR ONDE ESTÁ O UNDEFINED
-        let estoqueAtt =  itemNovo[0].qtd_estoque
-        let precoVendaAtt = itemNovo[0].preco_venda
-        let precoPromoAtt = itemNovo[0].preco_promocao
+        let estoqueAtt =  itemAtual[0].qtd_estoque
+        let precoVendaAtt = itemAtual[0].preco_venda
+        let precoPromoAtt = itemAtual[0].preco_promocao
 
          if (parseFloat(i.qtd_estoque) != parseFloat(estoqueAtt)) {
-            console.log(`vou atualizar o ${JSON.stringify(itemNovo)}`)
-            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
+            await estoque.update({ codigo_barras: i.codigo_barras, preco_venda: i.preco_venda, preco_promocao: i.preco_promocao, qtd_estoque: i.qtd_estoque })
             return
          }
 
          if (parseFloat(i.preco_venda) != parseFloat(precoVendaAtt)) {
-            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
+            await estoque.update({ codigo_barras: i.codigo_barras, preco_venda: i.preco_venda, preco_promocao: i.preco_promocao, qtd_estoque: i.qtd_estoque })
             return
          }
 
          if (parseFloat(i.preco_promocao) != parseFloat(precoPromoAtt)) {
-            await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: precoVendaAtt, preco_promocao: precoPromoAtt, qtd_estoque: estoqueAtt })
+            await estoque.update({ codigo_barras: i.codigo_barras, preco_venda: i.preco_venda, preco_promocao: i.preco_promocao, qtd_estoque: i.qtd_estoque })
             return
          }
 
