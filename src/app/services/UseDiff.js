793b2d1acfda1e132d4a43eec7df4603b9a63182
breitenbach <cesar.breitenbach@gmail.com>
2020-10-02 22:31:04 -0300

    correcoes de atualizacao

--- src/app/services/UseDiff.js
+++ src/app/services/UseDiff.js
@@ -5,13 +5,11 @@ const UseDiff = {
       const estoque = useEstoque()
      const fuckingPromise = tabelaAntiga.map( async (i,k) =>{
          const itemNovo = tabelaAtualizada.filter(novo_item => novo_item.codigo_barras == i.codigo_barras);
-         console.log(`Item novo: ${JSON.stringify(itemNovo)} - item velho: ${JSON.stringify(i)}`)
 
          if (!itemNovo){
             return
          }
 
-         console.log("AChei item novo: "+JSON.stringify(itemNovo))
 
          if (parseFloat(i.qtdestoque) != parseFloat(itemNovo[0].qtd_estoque)) {
             await estoque.update({ codigo_barras: itemNovo[0].codigo_barras, preco_venda: itemNovo[0].preco_venda, preco_promocao: itemNovo[0].preco_promocao, qtd_estoque: itemNovo[0].qtd_estoque })
