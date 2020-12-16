d49e92cbe976510e1b946f35e6c2a2ec3a44b574
breitenbach <cesar.breitenbach@gmail.com>
2020-12-16 19:34:37 -0300

    correcao cors

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -3,7 +3,7 @@ import File from '../models/File'
 class FileController {
     async store(req, res) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         const { originalname: name, filename: path } = req.file
 
         const imgId = await File.create({ name, path })
