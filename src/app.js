df9bff5727c54d75f541e123a2a5f453fe1484e3
breitenbach <cesar.breitenbach@gmail.com>
2020-12-16 19:29:07 -0300

    correcao cors

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ class App {
 
       this.server.use(cors({
          "origin": "*",
-         "allowedHeaders": ['Content-Type', 'auth', '*'],
          "methods": "GET,HEAD,PUT,POST,DELETE",
          "preflightContinue": false,
          "optionsSuccessStatus": 204
--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,13 +1,14 @@
 import File from '../models/File'
 
 class FileController {
-   async store (req, res) {
+    async store(req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        const { originalname: name, filename: path } = req.file
 
-      const {originalname: name, filename: path} = req.file
-
-      const  imgId = await File.create({name, path})
-      return res.json({imgId})
-   }
+        const imgId = await File.create({ name, path })
+        return res.json({ imgId })
+    }
 }
 
 export default new FileController()
