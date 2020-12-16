8967c1fa655ca58bf39cc0a71935705a79f3473f
breitenbach <cesar.breitenbach@gmail.com>
2020-12-16 18:51:30 -0300

    correcao cors

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -6,8 +6,6 @@ class FileController {
       const {originalname: name, filename: path} = req.file
 
       const  imgId = await File.create({name, path})
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Request-Width, Content-Type, Accept");
       return res.json({imgId})
    }
 }
