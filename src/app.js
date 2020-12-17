70dd63a832a1a377279786e7243fa0311a58e7e3
breitenbach <cesar.breitenbach@gmail.com>
2020-12-16 21:13:59 -0300

    correcao cors

--- src/app.js
+++ src/app.js
@@ -21,8 +21,8 @@ class App {
    middlewares() {
 
       this.server.use(cors({
+          origin: "*",
           allowedHeaders:"*",
-         origin: "*",
          "methods": "GET,HEAD,PUT,POST,DELETE",
          "preflightContinue": false,
          "optionsSuccessStatus": 204
