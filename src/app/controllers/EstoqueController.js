f1cd593c538e0f8599cf0647dd0964a3f87bc9fc
breitenbach <cesar.breitenbach@gmail.com>
2020-12-26 10:30:55 -0300

    estoque controller

--- src/app/controllers/EstoqueController.js
+++ src/app/controllers/EstoqueController.js
@@ -3,71 +3,95 @@ const { Op, QueryTypes } = require("sequelize");
 
 class EstoqueController {
 
-   async show(req, res){
-      const {idloja, idproduto} = req.params
-      try{
-         const estoque = await Estoque.findAll({
-            where: {
-                [Op.and]: [
-                  { id_loja: idloja },
-                  { id_produto:idproduto }
-                ]
-              }
+    async show(req, res) {
+        const { idloja, idproduto } = req.params
+        try {
+            const estoque = await Estoque.findAll({
+                where: {
+                    [Op.and]: [
+                        { id_loja: idloja },
+                        { id_produto: idproduto }
+                    ]
+                }
             })
-   
-         if(!estoque){
-            return res.status(400).json({error:"Não encontrei estoque"})
-         }
-   
-         return res.json(estoque)
 
-      }catch(e){  
-         console.log(e.message)
-      }
-   }
+            if (!estoque) {
+                return res.status(400).json({ error: "Não encontrei estoque" })
+            }
 
-   async store(req, res){
-    if (!req.userAdmin){
-        return res.json({error:"Você não é administrador."})
-     }
-      const {id_loja, codigo_produto, codigo_barras, qtd_estoque, preco_venda, preco_promocao, fabricante} = req.body
-      console.log(JSON.stringify(req.body))
-      try{
-         const estoque = await Estoque.create({id_loja, id_produto:codigo_produto, codigo_barras, qtd_estoque, preco_venda, preco_promocao, ativo:1, fabricante});
-         if(!estoque){
-            return res.status(400).json({error:"Não pude criar esse estoque"})
-         }
-         const {id} = estoque;
-         return res.status(201).json({success:"Estoque inserido com sucesso", estoque:{id, id_loja, codigo_produto, codigo_barras, qtd_estoque, preco_venda, preco_promocao}}) 
-      }catch (e){
-         console.log(e.message)
-      }
-   }
+            return res.json(estoque)
 
-   async update(req, res){
-       
-    if (!req.userAdmin){
-        return res.json({error:"Você não é administrador."})
-     }
-      const {idloja, idproduto} = req.params; 
-      const {codigo_barras, qtd_estoque, preco_venda, preco_promocao, ativo} = req.body
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
 
-     const sql = "select id from estoque where id_loja = :idloja and id_produto = :idproduto";
+    async store(req, res) {
+        if (!req.userAdmin) {
+            return res.json({ error: "Você não é administrador." })
+        }
+        const { id_loja, codigo_produto, codigo_barras, qtd_estoque, preco_venda, preco_promocao, fabricante } = req.body
+        console.log(JSON.stringify(req.body))
+        try {
+            const estoque = await Estoque.create({ id_loja, id_produto: codigo_produto, codigo_barras, qtd_estoque, preco_venda, preco_promocao, ativo: 1, fabricante });
+            if (!estoque) {
+                return res.status(400).json({ error: "Não pude criar esse estoque" })
+            }
+            const { id } = estoque;
+            return res.status(201).json({ success: "Estoque inserido com sucesso", estoque: { id, id_loja, codigo_produto, codigo_barras, qtd_estoque, preco_venda, preco_promocao } })
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
 
-     const estoque = await Estoque.sequelize.query(sql, {
-         type:QueryTypes.SELECT,
-         replacements:{
-             idloja,
-             idproduto
-         }
-     }) 
+    async update(req, res) {
 
-     console.log("Este é o estoque desse produto: "+ JSON.stringify(estoque));
+        if (!req.userAdmin) {
+            return res.json({ error: "Você não é administrador." })
+        }
+        const { idloja, idproduto } = req.params;
+        const { codigo_barras, qtd_estoque, preco_venda, preco_promocao, ativo, fabricante } = req.body
 
+        try {
+            const sql = "select id from estoque where id_loja = :idloja and id_produto = :idproduto";
 
-      }catch (e){
-         console.log(e.message)
-      }
+            const {id} = await Estoque.sequelize.query(sql, {
+                type: QueryTypes.SELECT,
+                replacements: {
+                    idloja,
+                    idproduto
+                }
+            })
+
+            if (!id){
+                return res.status(400).json({error:"Não encontrei estoque para esse produto"})
+            }
+
+            const sqlUpdate = `update estoque set codigo_barras = :codigo_barras, qtd_estoque = :qtd_estoque, preco_venda = : preco_venda
+                                            , preco_promocao = :preco_promocao, fabricante=:fabricante status = :status  where id = :id`
+            const updateEstoque = await Estoque.sequelize.query(sqlUpdate, {
+                type:QueryTypes.UPDATE,
+                replacements:{
+                    id,
+                    codigo_barras,
+                    qtd_estoque,
+                    preco_venda,
+                    preco_promocao,
+                    ativo,
+                    fabricante
+                }
+            })
+
+            if(!updateEstoque){
+                return res.status(400).json({error: "Não consegui atualizar estoque"})
+            }
+
+            return res.status(200).json(updateEstoque)
+            
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
 }
 
 export default new EstoqueController()
