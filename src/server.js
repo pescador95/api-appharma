6c847330f96d7a94182e3099996baec3f772f4ff
breitenbach <cesar.breitenbach@gmail.com>
2021-01-17 14:28:46 -0300

    socket no server http

--- src/server.js
+++ src/server.js
@@ -9,21 +9,49 @@ const io = require('socket.io')
 let existe = fs.existsSync(resolve(__dirname, 'config', 'privkey.pem'));
 
 if (existe) {
-    console.log("Existe chave...")
-    const options = {
-        key: fs.readFileSync(resolve(__dirname, 'config', 'privkey.pem'), 'utf-8'),
-        cert: fs.readFileSync(resolve(__dirname, 'config', 'fullchain.pem'), 'utf-8')
-    }
+   console.log("Existe chave...")
+   const options = {
+      key: fs.readFileSync(resolve(__dirname, 'config', 'privkey.pem'), 'utf-8'),
+      cert: fs.readFileSync(resolve(__dirname, 'config', 'fullchain.pem'), 'utf-8')
+   }
 
-    const server = https.createServer(options, app)
-        .listen(port, () => {
-            console.log(`enviroment: ${process.env.APP_ENV}`)
-            console.log(`estamos online na porta  ${process.env.APP_PORT}`)
-        })
+   const server = https.createServer(options, app)
+      .listen(port, () => {
+         console.log(`enviroment: ${process.env.APP_ENV}`)
+         console.log(`estamos online na porta  ${process.env.APP_PORT}`)
+      })
 
-    const socket = io(server)
+   const socket = io(server)
 
-    socket.on('connection', (sock) => {
+   socket.on('connection', (sock) => {
+      console.log("Recebendo conexão do cliente...")
+
+      sock.on('venda-recebida', (codigo_venda) => {
+         console.log(`Venda recebida, codigo: ${codigo_venda}`)
+         sock.codigo_venda = codigo_venda
+
+         console.log('isso é o codigo no sock: ' + sock.codigo_venda)
+
+         console.log(`enviando uma mensagem para o retaguarda... `)
+         socket.emit('tem-venda', sock.codigo_venda)
+      })
+
+   })
+
+
+
+} else {
+
+   console.log("Não existe chave...")
+   const server = http.createServer(app)
+
+      .listen(port, () => {
+         console.log(`enviroment: ${process.env.APP_ENV}`)
+         console.log(`estamos online na porta  ${port}`)
+      })
+
+      const socket = io(server);
+      socket.on('connection', (sock) => {
         console.log("Recebendo conexão do cliente...")
 
         sock.on('venda-recebida', (codigo_venda) => {
@@ -38,32 +66,4 @@ if (existe) {
 
     })
 
-
-
-} else {
-
-    console.log("Não existe chave...")
-    const server = http.createServer(app)
-
-        .listen(port, () => {
-            console.log(`enviroment: ${process.env.APP_ENV}`)
-            console.log(`estamos online na porta  ${port}`)
-        })
-
-    const socket = io(server)
-
-    socket.on('connection', (sock) => {
-        console.log("Recebendo conexão do cliente...")
-
-        sock.on('venda-recebida', (codigo_venda) => {
-            console.log(`Venda recebida, codigo: ${codigo_venda}`)
-            sock.codigo_venda = codigo_venda
-
-            console.log('isso é o codigo no sock: ' + sock.codigo_venda)
-
-            console.log(`enviando uma mensagem para o retaguarda... `)
-            socket.emit('tem-venda', sock.codigo_venda)
-        })
-
-
-    }
+}
