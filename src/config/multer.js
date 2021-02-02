c051b964546b5034e590416efd542ed1dd4e29f6
breitenbach <cesar.breitenbach@gmail.com>
2021-02-02 11:34:00 -0300

    correcao do tamanho da chave

--- src/config/multer.js
+++ src/config/multer.js
@@ -7,7 +7,7 @@ export default {
    storage: multer.diskStorage({
       destination: resolve(__dirname, '..', '..',  'tmp', 'uploads'),
       filename: (req, fil, cb) =>{
-         crypto.randomBytes(16, (err, res) =>{
+         crypto.randomBytes(24, (err, res) =>{
             if(err) return cb(err)
             return cb(null, res.toString('hex') + extname(fil.originalname))
          })
