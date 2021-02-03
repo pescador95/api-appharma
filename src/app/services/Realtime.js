8f8f82f5e04d388e75db88aad0fe3363a9459c9a
breitenbach <cesar.breitenbach@gmail.com>
2021-02-03 13:34:18 -0300

    singin no firebase

--- src/app/services/Realtime.js
+++ src/app/services/Realtime.js
@@ -1,10 +1,21 @@
 const firebase = require('firebase-admin')
 require('@firebase/database')
+require("firebase/auth");
+
 // Initialize Firebase
 
 exports.confirmaVenda = async function () {
 
-    await firebase.auth().signInWithEmailAndPassword('sistemas@approachmobile.company', 123456);
+    try{
+        const user = await firebase.auth().signInWithEmailAndPassword('sistemas@approachmobile.company', '123456')
+        console.log(JSON.stringify(user))
+
+    } catch (error) {
+        console.log(JSON.stringify(error))
+    }
+
+
+
 
     const db = firebase.database().ref();
     let valor = await db.child('vendas').once('value')
