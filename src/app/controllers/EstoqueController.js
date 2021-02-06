699d80d75ddd3c29c6f1991e9af59cfce030177a
breitenbach <cesar.breitenbach@gmail.com>
2021-02-06 16:20:37 -0300

    correcao

--- src/app/controllers/EstoqueController.js
+++ src/app/controllers/EstoqueController.js
@@ -67,7 +67,7 @@ class EstoqueController {
             })
             console.log("Vou entrar no undefined" + JSON.stringify(estoque))
 
-            if(typeof(estoque) === 'undefined'){
+            if(typeof(estoque) === 'undefined' || !estoque){
 
 
                 const addEstoque = await Estoque.create({ id_loja:idloja, id_produto:idproduto, codigo_barras, qtd_estoque, preco_venda, preco_promocao, status: 1, fabricante });
