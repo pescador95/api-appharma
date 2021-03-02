76f3bcd18e67900f7769532007d34cc64b29eb04
breitenbach <cesar.breitenbach@gmail.com>
2021-03-02 02:11:16 -0300

    add o campo dt_nasc no modelo e no validator

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,76 +3,86 @@ import jwt from 'jsonwebtoken'
 
 class UserController {
 
-   async index(req, res) {
-      const { cpf } = req.params;
+    async index(req, res) {
+        const { cpf } = req.params;
 
-      const user = await User.findOne({
-         where: { cpf }
-      })
-      if (!user) {
-         return res.status(200).json({ error: "Usuario não existe" })
-      }
+        const user = await User.findOne({
+            where: { cpf }
+        })
+        if (!user) {
+            return res.status(200).json({ error: "Usuario não existe" })
+        }
 
-      return res.status(200).json({ success: "Existe um usuario", user: { id: user.id, nome: user.name, enderecos: user.UserAddresses, whatsapp: user.whatsapp, dt_nasc: user.dt_nasc } })
+        return res.status(200).json({
+            success: "Existe um usuario",
+            user: {
+                id: user.id,
+                nome: user.name,
+                enderecos: user.UserAddresses,
+                whatsapp: user.whatsapp,
+                dt_nasc: user.dt_nasc,
+                email: user.email
+            }
+        })
 
-   }
+    }
 
-   async store(req, res) {
+    async store(req, res) {
 
-      const { cpf, name, password, confirmPassword, whatsapp } = req.body
+        const { cpf, name, password, confirmPassword, whatsapp } = req.body
 
-      const exists = await User.findOne({ where: { cpf } })
+        const exists = await User.findOne({ where: { cpf } })
 
-      if (exists) {
-         return res.status(401).json({ error: "Usuario já existe" })
-      }
+        if (exists) {
+            return res.status(401).json({ error: "Usuario já existe" })
+        }
 
 
-      const { id, admin, superadmin } = await User.create({ cpf, name, password, confirmPassword, whatsapp })
+        const { id, admin, superadmin } = await User.create({ cpf, name, password, confirmPassword, whatsapp })
 
-      const token = jwt.sign({ id, name, admin, superadmin }, process.env.SECRET, {  })
+        const token = jwt.sign({ id, name, admin, superadmin }, process.env.SECRET, {})
 
-      return res.status(201).json({
-         id,
-         cpf,
-         name,
-         token,
-         msg: "success"
-      })
+        return res.status(201).json({
+            id,
+            cpf,
+            name,
+            token,
+            msg: "success"
+        })
 
-   }
+    }
 
-   async update(req, res) {
+    async update(req, res) {
 
-      const { email, password, oldpassword, name, whatsapp } = req.body
-      const id = req.userId
-      const user = await User.findByPk(id)
+        const { email, password, oldpassword, name, whatsapp } = req.body
+        const id = req.userId
+        const user = await User.findByPk(id)
 
-      if (!user) {
-         return res.status(400).json({ error: "Não autorizado" })
-      }
+        if (!user) {
+            return res.status(400).json({ error: "Não autorizado" })
+        }
 
-      if (email && email !== user.email) {
-         const exist = await User.findOne({ where: { email } })
-         if (exist) {
-            return res.status(400).json({ error: "Usuario ja existe" })
-         }
-      }
+        if (email && email !== user.email) {
+            const exist = await User.findOne({ where: { email } })
+            if (exist) {
+                return res.status(400).json({ error: "Usuario ja existe" })
+            }
+        }
 
-      if (password) {
-         if (!oldpassword) {
-            return res.status(401).json({ error: "Senha antiga não confere" })
-         }
-         if (!(await user.checkPassword(oldpassword))) {
-            return res.status(401).json({ error: "Senha antiga não confere" })
-         }
-      }
+        if (password) {
+            if (!oldpassword) {
+                return res.status(401).json({ error: "Senha antiga não confere" })
+            }
+            if (!(await user.checkPassword(oldpassword))) {
+                return res.status(401).json({ error: "Senha antiga não confere" })
+            }
+        }
 
-      const updated = await user.update(req.body)
-      return res.json({ updated })
-   }
+        const updated = await user.update(req.body)
+        return res.json({ updated })
+    }
 
- 
+
 
 }
 export default new UserController();
